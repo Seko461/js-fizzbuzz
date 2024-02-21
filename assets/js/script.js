2c95a92cd9574101bf1b9f318622d9dc8b37a7e7
Seko461 <sekopierre@gmail.com>
2024-02-21 14:34:14 +0100

    first task done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,40 +4,21 @@
 // per i multipli di 5 stampi “Buzz”. 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per un altro ?
-//     Abbiamo visto qualcosa di particolare che possiamo usare ?
-
-
-
-
-
-
 for (let i = 1; i <= 100; i++) {
     console.log(i)
     if (i % 3 == 0) {
 
-        console.log(i + 'multiplo di 3');
+        console.log('Fizz');
     }
     else if (i % 5 == 0) {
 
-        console.log(i + 'multiplo di 5');
+        console.log('Buzz');
     }
     if (i % 3 == 0 && i % 5 == 0) {
 
-        console.log(i + 'multiplo di 3 e di 5');
+        console.log('FizzBuzz');
     }
 }
-
-
-//         Consigli del giorno:
-//  Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-//  Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro 
-//  programma così come lo faremmo "a mano"
-
-// : insetto: Ricordate di usare il console.log() sia in fase di preparazione 
-// e verifica dei dati che ovviamente durante il degugging per scovare 
-// potenziali errori!
 
 // BONUS 1:
 // Crea un container nel DOM, aggiungendo(attraverso la funzione append()) 
